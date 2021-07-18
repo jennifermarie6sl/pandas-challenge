a7f4ff5ff802078c473f45a842279958a09004dd
jennifermarie6sl <85765364+jennifermarie6sl@users.noreply.github.com>
2021-07-18 12:37:13 -0700

    First bucket working

--- HeroesOfPymoli/HeroesOfPymoli.py
+++ HeroesOfPymoli/HeroesOfPymoli.py
@@ -7,5 +7,6 @@ file_to_load = os.path.join(".", "Desktop", "pandas-challenge", "HeroesOfPymoli"
 # Read Purchasing File and store into Pandas data frame
 purchase_data = pd.read_csv(file_to_load)
 
-total_players = purchase_data["SN"].count() #counts total in column
-print(total_players)
+#Unique players plus their count:
+count_unique_players_df = len(purchase_data["SN"].unique())#returns count of times a unique item
+print(count_unique_players_df)
